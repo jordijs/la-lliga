1908d1fe1817be804f3d577183d456f3e3802e09
Jordi Julià <jordijs6@gmail.com>
2023-02-07 13:21:18 +0100

    Created relations game-team

---
+++ SoccerLeague-Project/app/Models/Team.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Team extends Model
+{
+    use HasFactory;
+
+    //One to Many
+    public function games(){
+        return $this->hasMany('App\Models\Game');   }
+}
