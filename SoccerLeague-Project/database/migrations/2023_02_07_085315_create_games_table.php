29eca1fe83bd0b3bb6768619008042c90f55fb38
Jordi Julià <jordijs6@gmail.com>
2023-02-07 10:50:07 +0100

    Migration for games

--- SoccerLeague-Project/database/migrations/2023_02_07_085315_create_games_table.php
+++ SoccerLeague-Project/database/migrations/2023_02_07_085315_create_games_table.php
@@ -15,9 +15,17 @@ return new class extends Migration
     {
         Schema::create('games', function (Blueprint $table) {
             $table->id();
-            $table->dateTime("date_time");
-            $table->tinyInteger("goals_local");
-            $table->tinyInteger("goals_visitor");
+            $table->unsignedBigInteger('team_local_id');
+            $table->foreign('team_local_id')
+            ->references('id')
+            ->on('teams');
+            $table->unsignedBigInteger('team_visitor_id');
+            $table->foreign('team_visitor_id')
+            ->references('id')
+            ->on('teams');
+            $table->dateTime('date_time');
+            $table->tinyInteger('goals_local');
+            $table->tinyInteger('goals_visitor');
             $table->timestamps();
         });
     }
