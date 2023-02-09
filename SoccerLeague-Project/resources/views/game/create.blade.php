ebe25b3abb9301fdc5fb7e2877c848e2e3c5b5f5
Jordi Julià <jordijs6@gmail.com>
2023-02-09 20:37:27 +0100

    Bootstrap->Tailwind on game/create

--- SoccerLeague-Project/resources/views/game/create.blade.php
+++ SoccerLeague-Project/resources/views/game/create.blade.php
@@ -5,17 +5,17 @@
 @endsection
 
 @section('content')
-    <section class="content container-fluid">
-        <div class="row">
-            <div class="col-md-12">
+    <section class="content container mx-auto sm:px-4 max-w-full mx-auto sm:px-4">
+        <div class="flex flex-wrap ">
+            <div class="md:w-full pr-4 pl-4">
 
                 @includeif('partials.errors')
 
-                <div class="card card-default">
-                    <div class="card-header">
-                        <span class="card-title">Create Game</span>
+                <div class="relative flex flex-col min-w-0 rounded break-words border bg-white border-1 border-gray-300 card-default">
+                    <div class="py-3 px-6 mb-0 bg-gray-200 border-b-1 border-gray-300 text-gray-900">
+                        <span class="mb-3">Create Game</span>
                     </div>
-                    <div class="card-body">
+                    <div class="flex-auto p-6">
                         <form method="POST" action="{{ route('games.store') }}"  role="form" enctype="multipart/form-data">
                             @csrf
 
