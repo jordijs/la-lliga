881f9d3e515a9a2a4ecf03cbfd1f46630c332dd5
Jordi Julià <jordijs6@gmail.com>
2023-02-09 13:01:01 +0100

    Converting bootstrap styles to tailwind on Teams
    Missing Create

--- SoccerLeague-Project/resources/views/team/form.blade.php
+++ SoccerLeague-Project/resources/views/team/form.blade.php
@@ -1,19 +1,19 @@
 <div class="box box-info padding-1">
     <div class="box-body">
         
-        <div class="form-group">
+        <div class="mb-4">
             {{ Form::label('name') }}
             {{ Form::text('name', $team->name, ['class' => 'form-control' . ($errors->has('name') ? ' is-invalid' : ''), 'placeholder' => 'Name']) }}
-            {!! $errors->first('name', '<div class="invalid-feedback">:message</div>') !!}
+            {!! $errors->first('name', '<div class="hidden mt-1 text-sm text-red">:message</div>') !!}
         </div>
-        <div class="form-group">
+        <div class="mb-4">
             {{ Form::label('score') }}
             {{ Form::text('score', $team->score, ['class' => 'form-control' . ($errors->has('score') ? ' is-invalid' : ''), 'placeholder' => 'Score']) }}
-            {!! $errors->first('score', '<div class="invalid-feedback">:message</div>') !!}
+            {!! $errors->first('score', '<div class="hidden mt-1 text-sm text-red">:message</div>') !!}
         </div>
 
     </div>
     <div class="box-footer mt20">
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline bg-blue-600 text-white hover:bg-blue-600">Submit</button>
     </div>
 </div>
